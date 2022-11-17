16069102006846999409dc66936cf8cfcd736845
Valentino Abad Giacomelli <valprogamer25@gmail.com>
2022-11-17 21:02:16 +0100

    Ejeccios 10 y 11 javascript completados

---
+++ ES6inicio10.js
@@ -0,0 +1,4 @@
+let numero1= prompt("Introduce el primer numero");
+let numero2= prompt("Introduce el segundo numero");
+let resultado= numero1 - numero2;
+alert("El resultado de la resta es " + resultado);
---
+++ ES6inicio11.js
@@ -0,0 +1,5 @@
+let numero1= prompt("Introduce el primer numero");
+let numero2= prompt("Introduce el segundo numero");
+let resultado_resta= numero1 - numero2;
+let resultado_multiplicacion= numero1 * numero2;
+alert("El resultado de la resta es " + resultado_resta + ". El resultado de la multiplicacion es " + resultado_multiplicacion);
